28ca635b366103b14e44c8471009685ca8618097
Angie Drucks <angie.drucks@gmail.com>
2024-11-20 11:15:33 +0100

    Added files for assignment 06

---
+++ web-tech06/ex4/products.js
@@ -0,0 +1,11 @@
+
+
+let container = document.getElementById('productContainer');
+let productURL = "http://localhost:8080/ex4/products.json"; // adapt to your real location!
+
+initialize(productURL,container);
+
+function initialize(jsonURL, domElement) {
+	// This function should retrieve all products from jsonURL and render them
+	// in the same way as on the last assignment sheet inside of domElement
+}
